f9a2ced3b8141ee23e2d2b6fae7b45802559be18
Larry Lee <llee454@gmail.com>
2020-06-04 15:20:56 +0000

    Added additional FAQs, removed the curly brace alias because it prevented me from inserting unescaped Mustache tags.

--- index-mustache-vars.js
+++ index-mustache-vars.js
@@ -1,27 +1,156 @@
 module.exports = {
   "endorsements?": [
-      {
-        "endorsement-name": "Murali Vijayaraghavan",
-        "endorsement-title": "Proof Engineer",
-        "endorsement-email": "vmurali@csail.mit.edu",
-        "endorsement-quote": "I was Larry's supervisor at SiFive. There, he was instrumental in bringing up a formal specification of the RISC-V processor in Kami, which is a framework in Coq to specify hardware circuits using transactional semantics. The task was pretty complex, given the enormity of the RISC-V specification and the ability to understand, Kami, a new framework completely, in order to write programs in that framework. Larry was a quick learner and was highly motivated to perform this task. He paid a lot of attention to detail and had excellent software engineering skills - always coming up with unit test cases even for the smallest change made or feature added to the code base. Overall, I enjoyed working with Larry.",
-        "endorsement-note": "Recieved May 13 2020 over email."
-      },
-      {
-        "endorsement-name": "Anthony Machado",
-        "endorsement-title": "Proof Engineer",
-        "endorsement-email": "tj.machado@gmail.com",
-        "endorsement-quote": "Working with Larry for nearly two years at SiFive I have come to know him as a man with a efficient and effective proof writing technique. As evidenced by his work in the ProcKami repository, he also shows the great attention to detail and forward planning one associates with well-written COQ code.",
-        "endorsement-note": "Recieved May 13 2020 over LinkedIn."
-      },
-      {
-        "endorsement-name": "Kenneth Roe",
-        "endorsement-title": "Proof Engineer",
-        "endorsement-email": "http://www.cs.jhu.edu/~roe/",
-        "endorsement-quote": "Larry Lee [has] great knowledge of theorem proving as well as the ProcKami [RISC-V] processor. He was always the one I talked to when I had a question on how something worked in the ProcKami processor or a question on the Kami language semantics. It was really enjoyable working with Larry Lee while I was at SiFive.",
-        "endorsement-note": "Recieved May 14 2020 over LinkedIn."
-      }
-    ],
+    {
+      "endorsement-name": "Murali Vijayaraghavan",
+      "endorsement-title": "Proof Engineer",
+      "endorsement-email": "vmurali@csail.mit.edu",
+      "endorsement-quote": "I was Larry's supervisor at SiFive. There, he was instrumental in bringing up a formal specification of the RISC-V processor in Kami, which is a framework in Coq to specify hardware circuits using transactional semantics. The task was pretty complex, given the enormity of the RISC-V specification and the ability to understand, Kami, a new framework completely, in order to write programs in that framework. Larry was a quick learner and was highly motivated to perform this task. <b>He paid a lot of attention to detail and had excellent software engineering skills</b> - always coming up with unit test cases even for the smallest change made or feature added to the code base. Overall, I enjoyed working with Larry.",
+      "endorsement-note": "Recieved May 13 2020 over email."
+    },
+    {
+      "endorsement-name": "Anthony Machado",
+      "endorsement-title": "Proof Engineer",
+      "endorsement-email": "tj.machado@gmail.com",
+      "endorsement-quote": "Working with Larry for nearly two years at SiFive I have come to know him as a man with <b>[an] efficient and effective proof writing technique</b>. As evidenced by his work in the ProcKami repository, he also shows the great attention to detail and forward planning one associates with well-written COQ code.",
+      "endorsement-note": "Recieved May 13 2020 over LinkedIn."
+    },
+    {
+      "endorsement-name": "Kenneth Roe",
+      "endorsement-title": "Proof Engineer",
+      "endorsement-email": "http://www.cs.jhu.edu/~roe/",
+      "endorsement-quote": "Larry Lee [has] great knowledge of theorem proving as well as the ProcKami [RISC-V] processor. He was always the one I talked to when I had a question on how something worked in the ProcKami processor or a question on the Kami language semantics. It was really enjoyable working with Larry Lee while I was at SiFive.",
+      "endorsement-note": "Recieved May 14 2020 over LinkedIn."
+    }
+  ],
+  "employers": [
+    {
+      "employerId": "sifive",
+      "employerName": "SiFive",
+      "employerStart": 2018,
+      "employerEnd":   2020,
+      "employerPosition": "Sr. Engineer",
+      "employerSummary": "Developed an open source RISC-V processor, used Coq to formally verify components used within SiFive's S-series processors, and contributed to the developement of Kami, a hardware modeling language embedded within Coq."
+    },
+    {
+      "employerId": "elucid",
+      "employerName": "Elucid Solutions",
+      "employerStart": 2015,
+      "employerEnd":   2018,
+      "employerPosition": "Sr. Programmer Analyst",
+      "employerSummary": "Developed several websites using Drupal for several government agencies. Attained the highest Contractor Performance Assessment Reporting System (CPARS) rating, including \"exceptional\" ratings for \"quality,\" \"schedule,\" and \"management,\" for my work developing achp.gov. Was instrumental in helping Elucid Solutions attain a CMMI (v1.3) level 3 certification."
+    },
+    {
+      "employerId": "jhu",
+      "employerName": "Johns Hopkins Univeristy",
+      "employerStart": 2015,
+      "employerEnd":   2009,
+      "employerPosition": "Sr. Programmer Analyst",
+      "employerSummary": "Drupal developer for several high profile websites including Popline, the largest online database of scientific research articles concerning reproductive health; and mHealth Evidence, an online database of scholarly journal articles related to mobile health interventions."
+    },
+  ],
+  "systems": [
+    {
+      "systemId": "coq",
+      "systemName": "Coq",
+      "systemLevel": "Professional",
+      "systemKeywords": "formal verifification methods theorem proving gallina ltac",
+      "systemSummary": "Used to formally verify algorithms developed for SiFIve, contributed to the Coq Standard Library, and released packages on the Coq Package Index."
+    },
+    {
+      "systemId": "riscv",
+      "systemName": "RISC-V",
+      "systemLevel": "Professional",
+      "systemKeywords": "hardware computer processor assembly formal verifification methods theorem proving",
+      "systemSummary": "Developed Kami Processor, an open source RISC-V processor. Formalized the RISC-V specification. Thoroughly knowledgable about the RISC-V specification."
+    },
+    {
+      "systemId": "drupal",
+      "systemName": "Drupal",
+      "systemLevel": "Professional",
+      "systemKeywords": "web development drupal cms content management system",
+      "systemSummary": "Over 10 years of professional experience developing Drupal websites and modules. I have developed websites using Drupal 6, 7, and 8, for a variety of clients including Johns Hopkins University."
+    }
+  ],
+  "languages": [
+    {
+      "languageId": "gallina",
+      "languageName": "Gallina and Ltac",
+      "languageLevel": "Fluent",
+      "languageKeywords": "coq formal verification methods theorem proving agda isabelle hol",
+      "languageSummary": "Used to formally verify algorithms developed for SiFIve, contributed to the Coq Standard Library, and released packages on the Coq Package Index."
+    },
+    {
+      "languageId": "haskell",
+      "languageName": "Haskell",
+      "languageLevel": "Fluent",
+      "languageKeywords": "functional lazy",
+      "languageSummary": "Used to model peripheral devices while developing the Kami Processor at SiFive. I have experience debugging, profiling, and optimizing Haskell programs using GHC's analysis toolchain."
+    },
+    {
+      "languageId": "js",
+      "languageName": "JavaScript",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web nodejs node",
+      "langaugeSummary": "Developed a JavaScript framework similar to REACT-JS. Professional experience developing NodeJS based applications, and integrating REACT-JS with Drupal's theme system."
+    },
+    {
+      "languageId": "solidity",
+      "languageName": "Solidity",
+      "languageLevel": "Familiar",
+      "languageKeywords": "blockchain block chain crypto",
+      "languageSummary": "Have Read the Solidity language specification."
+    },
+    {
+      "languageId": "sass",
+      "languageName": "Sass",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web html xml css drupal php",
+      "languageSummary": "Extensive professional experience using Sass",
+    },
+    {
+      "languageId": "css",
+      "languageName": "CSS",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web html xml css drupal php",
+      "languageSummary": "Extensive professional experience using CSS",
+    },
+    {
+      "languageId": "html",
+      "languageName": "HTML",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web html xml css drupal php",
+      "languageSummary": "Extensive professional experience using HTML",
+    },
+    {
+      "languageId": "xml",
+      "languageName": "XML",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web html xml css drupal php",
+      "languageSummary": "Extensive professional experience using XML",
+    },
+    {
+      "languageId": "php",
+      "languageName": "PHP",
+      "languageLevel": "Fluent",
+      "languageKeywords": "web html xml css drupal php",
+      "languageSummary": "Over 10 years of experience developing Drupal based websites and modules. Drupal's back-end is written in PHP.",
+    },
+    {
+      "languageId": "scheme",
+      "languageName": "Scheme",
+      "languageLevel": "Familiar",
+      "languageKeywords": "functional lisp clojure",
+      "languageSummary": "Have read the R6RS and R7RS specifications and used Scheme on several informal projects."
+    },
+    {
+      "languageId": "ocaml",
+      "languageName": "OCaml",
+      "languageLevel": "Familiar",
+      "languageKeywords": "functional",
+      "languageSummary": "Coursework included the ML variant SML. I am familiar with OCaml primarily through my interactions with Coq, which is written in OCaml. Additionally, I have read the OCaml language Specification."
+    }
+  ], 
+
   "eqn": function () {
     return function (text, render) {
       var xs = (/([^:]*):(.*)/).exec (text.trim ()).map (function (x) {return x.trim (); });
